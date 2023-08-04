5e8ebecc70fdc4c767b4092f20dfcfac45cfabcd
ngenohkevin <ngenohkevin19@gmail.com>
2023-08-04 12:55:53 +0300

    useEffect hook to fetch houses

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,7 +1,19 @@
 import './main-page.css';
+import { useEffect } from 'react';
 import Header from './header';
 
 function App() {
+
+  useEffect(() => {
+    const fetchHouses = async () => {
+      const resp = await fetch("/houses.json ")
+      const houses = await resp.json()
+    }
+    fetchHouses();
+
+  }, [])
+  
+
   return (
     <div className="container">
       <Header subtitle="Providing houses all over the world"/>
